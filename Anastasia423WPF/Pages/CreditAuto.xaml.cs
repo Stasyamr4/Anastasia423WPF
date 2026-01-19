7dfd6320a4aca45d5ff211dca2bdc65ba7cc8986
Stasyam4 <anastaiamura04@mail.ru>
2026-01-19 15:07:55 +0300

    да

--- Anastasia423WPF/Pages/CreditAuto.xaml.cs
+++ Anastasia423WPF/Pages/CreditAuto.xaml.cs
@@ -20,13 +20,23 @@ namespace Anastasia423WPF.Pages
     /// </summary>
     public partial class CreditAuto : Page
     {
+        Car ItsCar;
         public CreditAuto(Car car1)
         {
+            ItsCar = car1;
             InitializeComponent();
             
         }
         double SrokCredit = 0;
         double ProcCred = 0;
+        decimal A;
+        public void CreditCalculate()
+        {
+            decimal S = ItsCar.Price - (ItsCar.Price * (decimal)(ProcCred / 100));
+            int i = (int)22.5/12;
+            A = S * ((i * (1 + i) ^ ((int)SrokCredit)) / (1 + i) ^ (int)(SrokCredit - 1));
+        }
+
         private void SrokCred_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             //YearText.Visibility = Visibility.Visible;
@@ -39,15 +49,23 @@ namespace Anastasia423WPF.Pages
         private void SrokCred_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
         {
             YearText.Visibility = Visibility.Visible;
-            SrokCredit = SrokCred.Value;
+            YearText.Text = "Выбранный срок кредита (в месяцах): ";
+            SrokCredit = Math.Round(SrokCred.Value);
             YearText.Text += SrokCredit.ToString();
         }
 
         private void PercentCred_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
         {
+
             PercentText.Visibility = Visibility.Visible;
-            ProcCred = PercentCred.Value;
+            PercentText.Text = "Выбранный процент кредита: ";
+            ProcCred = Math.Round(PercentCred.Value);
             PercentText.Text += ProcCred.ToString();
+            if (SrokCredit != 0 && ProcCred != 0)
+            {
+                CreditCalculate();
+                MonthPay.Text = $"Ваш ежемесячный платеж составляет {A.ToString()}";
+            }
         }
     }
 }
