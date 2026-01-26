599ca7d210709aea276275c395a95a423f19a001
Stasyam4 <anastaiamura04@mail.ru>
2026-01-26 15:52:38 +0300

    первая страница готова на 70%

---
+++ Anastasia423WPF/Pages/Products.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Anastasia423WPF.NewFolder1
+{
+    /// <summary>
+    /// Логика взаимодействия для Products.xaml
+    /// </summary>
+    public partial class Products : Page
+    {
+        public Products()
+        {
+            InitializeComponent();
+            AddText();
+        }
+
+
+        public void AddText()
+        {
+            var prod = new Product();
+            prod = Core.Context.Product.Where(p => p.name == "шарф").FirstOrDefault();
+            Sharf.Text = $"{prod.name}\n {prod.price}";
+
+            prod = Core.Context.Product.Where(p => p.name == "палочка").FirstOrDefault();
+            Palochka.Text = $"{prod.name}\n {prod.price}";
+
+            prod = Core.Context.Product.Where(p => p.name == "мантия").FirstOrDefault();
+            MantiaNevidimka.Text = $"{prod.name}\n {prod.price}";
+
+            prod = Core.Context.Product.Where(p => p.name == "учебник").FirstOrDefault();
+            Book.Text = $"{prod.name}\n {prod.price}";
+        }
+    }
+}
